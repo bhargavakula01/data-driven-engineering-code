9dd84bb684d0a351e340c5b9ebf617503d4d96d7
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-05-11 16:53:19 -0400

    writing julia example for multi-linear regression

--- multi_linear_regression/multi_linear_regression_example.jl
+++ multi_linear_regression/multi_linear_regression_example.jl
@@ -1,1 +1,12 @@
 # Example of Multi-Linear Regression using Julia
+
+
+import Pkg
+Pkg.add("RDatasets")
+Pkg.add("DataFrames")
+
+using RDatasets, DataFrames
+
+# loading the 'hald' dataset
+hald = dataset("datasets", "Hald")
+
