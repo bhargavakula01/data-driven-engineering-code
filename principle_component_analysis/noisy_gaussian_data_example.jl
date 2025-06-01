63e899f06d7ade706956dc9c4c1f416e65ba73ea
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-06-01 12:47:34 -0400

    switched rand() to randn() to use normal distribution when getting random values for X

--- principle_component_analysis/noisy_gaussian_data_example.jl
+++ principle_component_analysis/noisy_gaussian_data_example.jl
@@ -21,12 +21,12 @@ x_c = [2; 1] # center of data
 #([cos(θ) -sin(θ); sin(θ) cos(θ)]) rotates counter-clockwise by θ
 R = [cos(θ) sin(θ); -sin(θ) cos(θ)] 
 
-npts = 10000 #number of data points
+npts = 1000 #number of data points
 
 #The 'rand(Float64,(npts, 2))*Diagonal(σ)*R' applies the rotation of the data 
-#and 'ones(npts, 2)*Diagonal(x_c)' adds the centering of the data at (2,1)  
-X = rand(Float64,(npts, 2))*Diagonal(σ)*R + ones(npts, 2)*Diagonal(x_c) #(10000,2) matrix
-
+#and 'ones(npts, 2)*Diagonal(x_c)' adds the centering of the data at (2,1)
+X = randn(npts, 2)*Diagonal(σ)*R + ones(npts, 2)*Diagonal(x_c) #(10000,2) matrix
+scatter(X[:,1], X[:, 2])
 ###COMPUTE PRINCIPLE COMPONENTS USING PCA & PLOT CONFIDENCE INTERVALS:
 x_avg = mean(X, dims=1)
 
@@ -42,7 +42,6 @@ S_diagonal = Diagonal(S)
 X_std = [cos.(θ) sin.(θ)]*S_diagonal*V'
 
 #plotting:
-scatter(X[:,1], X[:, 2])
 plot!(x_avg[1] .+ X_std[:,1], x_avg[2] .+ X_std[:,2], color=:red, linestyle=:solid)
 plot!(x_avg[1] .+ 2*X_std[:,1], x_avg[2] .+ 2*X_std[:,2], color=:red, linestyle=:solid)
 plot!(x_avg[1] .+ 3*X_std[:,1], x_avg[2] .+ 3*X_std[:,2], color=:red, linestyle=:solid)
