79e67dd785d43b2ec4de40f74913652d1761ba64
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-06-01 12:27:01 -0400

    completed PCA example on noisy gaussian data

--- principle_component_analysis/noisy_gaussian_data_example.jl
+++ principle_component_analysis/noisy_gaussian_data_example.jl
@@ -13,22 +13,37 @@ using LinearAlgebra, Random, Plots, Statistics
 σ = [2; 0.5] # principle axis
 x_c = [2; 1] # center of data
 
-#need to apply rotation of the data (using the rotation matrix)
+#need to apply rotation of the data (using the rotation matrix):
 π = pi
 θ = π/3
-R = [cos(θ) -sin(θ); sin(θ) cos(θ)]
+#Some information regarding rotation matrices 
+#([cos(θ) sin(θ); -sin(θ) cos(θ)]) rotates clockwise by θ. 
+#([cos(θ) -sin(θ); sin(θ) cos(θ)]) rotates counter-clockwise by θ
+R = [cos(θ) sin(θ); -sin(θ) cos(θ)] 
 
 npts = 10000 #number of data points
 
-X = rand(Float64,(npts, 2))*Diagonal(σ)*R + ones(npts, 2)*Diagonal(x_c)
-
-#plotting
-scatter(X[:,1], X[:, 2])
+#The 'rand(Float64,(npts, 2))*Diagonal(σ)*R' applies the rotation of the data 
+#and 'ones(npts, 2)*Diagonal(x_c)' adds the centering of the data at (2,1)  
+X = rand(Float64,(npts, 2))*Diagonal(σ)*R + ones(npts, 2)*Diagonal(x_c) #(10000,2) matrix
 
 ###COMPUTE PRINCIPLE COMPONENTS USING PCA & PLOT CONFIDENCE INTERVALS:
 x_avg = mean(X, dims=1)
 
-#getting mean subtracted data (B)
+#getting mean subtracted data (B):
 B = X - ones(npts, 1)*x_avg
 
+#computing the SVD:
 U, S, V = svd(B/sqrt(npts))
+S_diagonal = Diagonal(S)
+
+θ = (0:0.01:1)*2*π
+#x = rcos(θ) & y = rsin(θ)
+X_std = [cos.(θ) sin.(θ)]*S_diagonal*V'
+
+#plotting:
+scatter(X[:,1], X[:, 2])
+plot!(x_avg[1] .+ X_std[:,1], x_avg[2] .+ X_std[:,2], color=:red, linestyle=:solid)
+plot!(x_avg[1] .+ 2*X_std[:,1], x_avg[2] .+ 2*X_std[:,2], color=:red, linestyle=:solid)
+plot!(x_avg[1] .+ 3*X_std[:,1], x_avg[2] .+ 3*X_std[:,2], color=:red, linestyle=:solid)
+savefig("noisy_gaussian_data_example.png")
