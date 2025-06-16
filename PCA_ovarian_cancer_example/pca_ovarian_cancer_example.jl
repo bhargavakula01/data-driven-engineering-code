232cecd058a9739e5144173a74e4cecb985922ef
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-06-15 21:33:05 -0400

    updating repo

--- PCA_ovarian_cancer_example/pca_ovarian_cancer_example.jl
+++ PCA_ovarian_cancer_example/pca_ovarian_cancer_example.jl
@@ -4,5 +4,8 @@ Pkg.add("LinearAlgebra")
 Pkg.add("Random")
 Pkg.add("Plots")
 Pkg.add("Statistics")
+Pkg.add("CSV")
 
-using LinearAlgebra, Random, Plots, Statistics
+using LinearAlgebra, Random, Plots, Statistics, CSV
+
+
