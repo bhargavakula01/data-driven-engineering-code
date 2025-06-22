fc618e84aa1ced0449f9c5435c8ea69cc9339a66
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-06-22 19:38:45 -0400

    working on the ovarian cancer example

--- PCA_ovarian_cancer_example/pca_ovarian_cancer_example.jl
+++ PCA_ovarian_cancer_example/pca_ovarian_cancer_example.jl
@@ -1,11 +1,12 @@
 # Ovarian cancer example (Julia)
-using Pkg
-Pkg.add("LinearAlgebra")
-Pkg.add("Random")
-Pkg.add("Plots")
-Pkg.add("Statistics")
-Pkg.add("CSV")
 
-using LinearAlgebra, Random, Plots, Statistics, CSV
+using Pkg, LinearAlgebra, Random, Plots, Statistics, CSV, DataFrames
 
 
+# importing the CSV:
+raw_data = CSV.read("data/ovariancancer.csv", DataFrame)
+
+numerical_data = raw_data[:, 1:end-1]
+
+U, S, V = svd(numerical_data)
+S_diagonal = Diagonal(S)
