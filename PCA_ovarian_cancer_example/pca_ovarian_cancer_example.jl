41666fe1264550ef93dc20d9103e6829edb4f164
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-07-12 16:08:52 -0400

    added code for ovarian cancer example

--- PCA_ovarian_cancer_example/pca_ovarian_cancer_example.jl
+++ PCA_ovarian_cancer_example/pca_ovarian_cancer_example.jl
@@ -2,11 +2,27 @@
 
 using Pkg, LinearAlgebra, Random, Plots, Statistics, CSV, DataFrames
 
-
 # importing the CSV:
 raw_data = CSV.read("data/ovariancancer.csv", DataFrame)
 
-numerical_data = raw_data[:, 1:end-1]
+σ = Matrix(raw_data[:, 1:end-1])
+group = raw_data[:,end]
 
-U, S, V = svd(numerical_data)
+U, S, V = svd(σ)
 S_diagonal = Diagonal(S)
+
+#Retrieving pts and plotting:
+plt = scatter3d()
+for i = 1:size(σ, 1)
+    x = transpose(V[:, 1]) * σ[i, :]
+    y = transpose(V[:, 2]) * σ[i, :]
+    z = transpose(V[:, 3]) * σ[i, :]
+
+    if(group[i] == "Cancer")
+        scatter3d!([x], [y], [z], marker=:star5)
+    else
+        scatter3d!([x], [y], [z], marker=:circle)
+    end
+end
+
+savefig("./Ovarian_Cancer.png")
