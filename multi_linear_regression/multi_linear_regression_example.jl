89100bfbda9eb095942f3aa10177eaf999acb087
Bhargav Akula <mail.bhargavakula@gmail.com>
2026-04-18 17:22:07 -0400

    updating main

--- multi_linear_regression/multi_linear_regression_example.jl
+++ multi_linear_regression/multi_linear_regression_example.jl
@@ -9,12 +9,12 @@ Pkg.add("LinearAlgebra")
 using CSV, DataFrames, LinearAlgebra
 
 # loading the 'hald' dataset
-hald_df = CSV.read("data/hald.csv", DataFrame)
+hald_df = CSV.read("./multi_linear_regression/data/hald.csv", DataFrame)
 
 println(hald_df)
 
-hald_ingredients_df = Matrix(hald_df[:, 1:4])
-hald_heat_df = hald_df[:, 5]
+hald_ingredients_df = Matrix(hald_df[:, 1:4]) #matrix A in Ax = b
+hald_heat_df = hald_df[:, 5] # matrix b in Ax = b
 
 println("Ingredients: $hald_ingredients_df")
 println("Heat: $hald_heat_df")
@@ -22,6 +22,6 @@ println("Heat: $hald_heat_df")
 U, S, V = svd(hald_ingredients_df) #performing an economic SVD by default when matrix isn't square
 S = Diagonal(S)
 
-x_approx = V*inv(S)*transpose(U)*hald_heat_df
+x_approx = V*inv(S)*transpose(U)*hald_heat_df # x = VΣ^{-1}U^{*}b
 
 println("x: $x_approx") # This is a vector --> A^{-1}b = x, where A is a matrix and b is a vector
